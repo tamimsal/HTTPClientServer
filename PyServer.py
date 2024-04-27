4a583ed0242baa2f3fefaa4aadd0c1094ac18a4b
Tamim Salhab <tamem222333@gmail.com>
2024-04-27 18:48:51 +0300

    adding flask with login

---
+++ PyServer.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, session
+from flask_basicauth import BasicAuth
+
+app = Flask(__name__)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'admin' and password == 'password':
+            return render_template('index.html')
+        else:
+            return render_template('unauthorized.html')
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
